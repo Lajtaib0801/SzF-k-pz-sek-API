abab5aeeb498f1ecb57117ca7dd8d4038f8b3e2e
Lajtaib0801 <lajtaibenja@gmail.com>
2023-11-14 09:37:53 +0100

    up

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const trainings = require('./routes/trainings')
 require('dotenv').config() // A .env fájlt olvassa
 const app = express()
 app.listen(process.env.PORT, () =>
-    console.log(`Server running on port ${process.env.PORT}`)
+    console.log(`Server is running on port ${process.env.PORT}`)
 )
+
+// const logger = (req, res, next) => {
+//     req.hello = 'Hello World!'
+//     console.log('Middleware ran.')
+//     next()
+// }
+// app.use(logger)
+app.use(morgan('dev'))
 
 app.use('/api/trainings', trainings)
