4b51e211dd42077422044c691a1c139e3a7c337f
Lajtaib0801 <lajtaibenja@gmail.com>
2023-11-21 09:30:57 +0100

    up

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -1,29 +1,39 @@
-const Training = require('../models/Training');
+const { default: mongoose } = require('mongoose')
+const Training = require('../models/Training')
 // @desc   Get all trainings
 // @route  GET /api/trainings
 // @access Public
-exports.getTrainings = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all trainings', hello: req.hello })
+exports.getTrainings = async (req, res, next) => {
+    try {
+        const trainings = await Training.find()
+        res.status(201).json({ success: true, data: trainings })
+    } catch (error) {
+        res.status(400).json({ success: false })
+    }
+
 } // @desc   Get single training
 // @route  GET /api/trainings/:id
 // @access Public
-exports.getTraining = (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        msg: `Get training ${req.params.id}`,
-    })
+exports.getTraining = async (req, res, next) => {
+    const {id: id} = req.params
+    try {
+        const training = await Training.findById(id)
+        res.status(201).json({ success: true, data: training })
+    } catch (error) {
+        res.status(400).json({ success: false })
+    }
 } // @desc   Create new training
 // @route  POST /api/trainings
 // @access Private
 
-    exports.createTraining = async (req, res, next) => {
-        try {
-            const training = await Training.create(req.body)
-            res.status(201).json({ success: true, data: training })
-        } catch (error) {
-            res.status(400).json({ success: false })
-        }
+exports.createTraining = async (req, res, next) => {
+    try {
+        const training = await Training.create(req.body)
+        res.status(201).json({ success: true, data: training })
+    } catch (error) {
+        res.status(400).json({ success: false })
     }
+}
 
 // @desc   Update training
 // @route  PUT /api/trainings/:id
